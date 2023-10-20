ba879c4428ef51350ccbdcebd1dde0f15c7cf788
Sherelle <sherellie1@hotmail.com>
2023-10-20 16:15:15 +0100

    initial app now works and passes initial test. Greet method is working. The build can now begin

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -1,5 +1,7 @@
 package com.cbfacademy.apiassessment;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class App {
-
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s", name);
+		// try (Scanner sc = new Scanner(System.in);) {
+		// 	System.out.println("What is your name?");
+		// String userName = sc.nextLine();
+		// 	return userName.isBlank() ? String.format("Hello %s", name) : String.format("Hello %s", userName);
+		// } catch (Exception e) {	
+		// 	System.out.println("Error occurred while greeting client.");
+		// }
+		// return greeting(name);
+	
+			return String.format("Hello %s", name);
 	}
 
 }
