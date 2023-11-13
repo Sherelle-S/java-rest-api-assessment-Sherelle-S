a400666c1cc584c07b9b2f2e6c65c48b53243bb6
Sherelle <sherellie1@hotmail.com>
2023-11-13 04:52:10 +0000

    readJsonData is now returning something, not the correct thing, but something

--- src/main/java/com/cbfacademy/apiassessment/controller/WatchlistController.java
+++ src/main/java/com/cbfacademy/apiassessment/controller/WatchlistController.java
@@ -1,15 +1,22 @@
 package com.cbfacademy.apiassessment.controller;
 
+import java.io.FileNotFoundException;
+
+import org.json.simple.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cbfacademy.apiassessment.deserialize.ReadFromJson;
 import com.cbfacademy.apiassessment.exceptions.FailedToIOWatchlistException;
+import com.cbfacademy.apiassessment.exceptions.JsonWatchlistParsingException;
 import com.cbfacademy.apiassessment.model.CreateWatchlist;
 import com.cbfacademy.apiassessment.model.Watchlist;
 import com.cbfacademy.apiassessment.serialize.SerializeWatchlist;
@@ -37,6 +44,13 @@ public class WatchlistController {
         this.serviceImpl = serviceImpl;
         this.writeToJsonFile = writeToJsonFile;
         this.watchlist = watchlist;
+    }
+
+    @GetMapping("/working")
+    public String responding() throws FileNotFoundException {
+        ReadFromJson readFile = new ReadFromJson();
+        return readFile.readJsonWatchlist();
+
     }
 
 
--- src/main/java/com/cbfacademy/apiassessment/deserialize/ReadFromJson.java
+++ src/main/java/com/cbfacademy/apiassessment/deserialize/ReadFromJson.java
@@ -1,15 +1,57 @@
 package com.cbfacademy.apiassessment.deserialize;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONAware;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.cbfacademy.apiassessment.exceptions.ItemNotFoundException;
+import com.cbfacademy.apiassessment.exceptions.JsonWatchlistParsingException;
 import com.cbfacademy.apiassessment.model.CreateWatchlist;
 import com.cbfacademy.apiassessment.serialize.WriteToJson;
 
 public class ReadFromJson {
+
+    private static final Logger log = LoggerFactory.getLogger(WriteToJson.class);
+
+
+    public String readJsonWatchlist() throws FileNotFoundException{
+        String jsonRepo = "JsonWatchlist.json";
+        try (FileReader reader = new FileReader("JsonWatchlist.json")) {
+            JSONParser jsonParser = new JSONParser();
+            Object obj = jsonParser.parse(reader);
+            
+            JSONArray deserializedList = (JSONArray) obj;
+            return deserializedList.toJSONString();
+            // if (obj instanceof JSONAware) {
+            //     return ((JSONAware) obj).toJSONString();
+            // } else {
+            //     // Handle the case when obj is not an instance of JSONAware
+            //     return obj.toString();
+            // }
+            // return deserializedList;
+        } catch (ParseException e) {
+            log.error("Exception ocurred during json parse to write file.", e);
+            // throw new JsonWatchlistParsingException("Exception ocurred at readJsonWatchlist when trying to parse file.", e);
+        } catch (FileNotFoundException e ){
+            log.error("File that we a re reading cannot be found", e);
+            throw new ItemNotFoundException(jsonRepo);
+        } catch (IOException e) {
+            log.error("Reader closed before resources had finished", e);
+        }
+        // return readJsonWatchlist();
+        return jsonRepo;
+    }
 
     // private static final Logger log = LoggerFactory.getLogger(WriteToJson.class);   
     // private CreateWatchlist createWatchlist;
